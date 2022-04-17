a199e50dd6dd00aceb134dbe14753e9f5d75892d
NelsonWang <wangnelson2@gmail.com>
2022-04-17 17:20:01 -0400

    links | added | folder

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Smoothtalkers',
   tagline: 'Public Speaking & Interviewing Skills',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://smooth.l0l.in',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
