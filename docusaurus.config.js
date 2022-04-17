51db5a1eed2fe16623f0a90d332c4f620c1380dc
NelsonWang <wangnelson2@gmail.com>
2022-04-16 23:57:59 -0400

    sm | config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'smoothtalker', // Usually your GitHub org/user name.
+  projectName: 'Smooth Talkers Docs', // Usually your repo name.
 
   presets: [
     [
