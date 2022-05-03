3a2040abcbce2b4e7b8f9785bea0b5b65df7067b
NelsonWang <wangnelson2@gmail.com>
2022-05-03 14:12:03 -0400

    Production update |

--- build/assets/js/1abab7c0.c56de15d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksmoothdocs=self.webpackChunksmoothdocs||[]).push([[788],{4469:function(s){s.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-blog","id":"default"}}')}}]);
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Smoothtalkers',
+  title: 'Smooth Talkers',
   tagline: 'Public Speaking & Interviewing Skills',
   url: 'https://smoothbc.com',
   baseUrl: '/',
